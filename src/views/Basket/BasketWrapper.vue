d7c8548e5c09e1bfb7e3332a3f20bb5c09289c0c
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:52:59 +0300

    Create BasketWrapper.vue
    firebase işlemleri

---
+++ src/views/Basket/BasketWrapper.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="basket-wrapper">
+      <div class="container-max-1580">
+        <!-- ... (other HTML content) ... -->
+        
+        <EmptyBasket></EmptyBasket>
+
+        <!-- <Checkout2></Checkout2>
+        <BasketLoader></BasketLoader> -->
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+//   import Checkout2 from './Checkout2.vue';
+ import EmptyBasket from './EmptyBasket.vue';
+//   import BasketLoader from './BasketLoader.vue';
+  
+  export default defineComponent({
+    components: {
+
+      EmptyBasket,
+
+    },
+  });
+  </script>
+  
