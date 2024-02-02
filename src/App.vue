3e5e9fcce1940ac84f2bce1c6d611cafe6a552ab
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:48:52 +0300

    Create App.vue
    firebase işlemleri

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+
+import {  RouterView } from 'vue-router'
+
+
+import HeaderC from './components/HeaderC.vue';
+import FooterC from './components/FooterC.vue';
+
+</script>
+
+<template>
+
+  <HeaderC msg="You did it!"/>
+
+  <RouterView />
+
+  <FooterC/>
+
+</template>
+
+<style scoped>
+
+</style>
