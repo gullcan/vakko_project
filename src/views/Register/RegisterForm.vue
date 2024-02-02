340cbfbb0aa664c778f952ce24249e1143eec864
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:53:25 +0300

    Create RegisterForm.vue
    firebase işlemleri

---
+++ src/views/Register/RegisterForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="usr">
+      <div class="usr-body">
+        <PzForm id="RegisterForm" class="register-form pz-form" service="User" action="register">
+        </PzForm>
+      </div>
+      <div class="usr-media" style="background-image: url(https://fbfad05d.cdn.akinoncloud.com/static_omnishop/shopi-28/img/main_slide_login_2.jpg);">
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">1
+  import { defineComponent } from 'vue';
+  import PzForm from './PzForm.vue';  // PzForm bileşeni
+
+  export default defineComponent({
+    components: {
+      PzForm
+    },
+  });
+  </script>
+  
