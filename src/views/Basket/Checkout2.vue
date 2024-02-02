3a0cf65209a3f6e1016fd7fa21204fe6ebcd3f82
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:53:03 +0300

    Create Checkout2.vue

---
+++ src/views/Basket/Checkout2.vue
@@ -0,0 +1,35 @@
+<template>  
+  <div class="checkout2">
+      <!-- ... (other HTML content) ... -->
+      <BasketHeader v-if="!basketHeaderHidden"></BasketHeader>
+      <div class="checkout-grid js-basket-content" v-if="!basketContentHidden">
+        <BasketItems></BasketItems>
+      </div>
+      <!-- ... (other HTML content) ... -->
+  </div>
+  
+</template>
+
+  
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import BasketHeader from './BasketHeader.vue';
+  import BasketItems from './BasketItems.vue';
+  
+  export default defineComponent({
+    components: {
+      BasketHeader,
+      BasketItems,
+    },
+    data() {
+      return {
+        basketHeaderHidden: true,
+        basketContentHidden: true,
+        // ... (other data properties) ...
+      };
+    },
+    // ... (other script methods) ...
+  });
+  </script>
+  
