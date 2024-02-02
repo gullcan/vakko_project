3336b0b4ea9ec8a284b71368badbd2631d8cd3db
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:51:34 +0300

    Create CerezC.vue
    firebase işlemleri

---
+++ src/components/CerezC.vue
@@ -0,0 +1,17 @@
+<template>
+    
+<div class="flatpage-cookie js-cookie-consent" hidden="">
+    <div class="flex-container">
+        <text class="cookie__title">ÇEREZ KULLANIMI</text>
+        <p class="cookie__text">
+            Sizlere daha iyi hizmet sunabilmek için web sitemizde çerezler kullanılmaktadır.
+            Detaylı bilgi için <a href="/cerez-politikasi/" target="_blank" class="more__info">tıklayınız.</a>
+        </p>
+        <button style="text-align: center;" class="cookie__button js-cookie-accept">
+            OKUDUM / KAPAT
+        </button>
+    </div>
+</div>
+
+</template>
+
