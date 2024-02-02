8bfd23f8c4b9c811245992b623ccbc81c609d8f8
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:52:52 +0300

    Create BasketHeader.vue
    firebase işlemleri

---
+++ src/views/Basket/BasketHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="basket-header js-basket-header" v-if="!hidden">
+      <div class="checkout2-header">
+        <div class="checkout2-heading">
+          <div class="checkout2-title">SEPET</div>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    props: {
+      hidden: {
+        type: Boolean,
+        default: true,
+      },
+    },
+  });
+  </script>
+  
