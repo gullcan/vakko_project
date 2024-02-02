850411e77f2663f7ccdd2952c1d81ba753b32da3
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:52:37 +0300

    Create main.ts
    firebase işlemleri

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import "./assets/css/style.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faBars,
+  faUser,
+  faMagnifyingGlass,
+  faBasketShopping,
+  faArrowLeft,
+  faXmark,
+  faGlobe,
+  faShieldHalved,
+  faTruck,
+  faRotateRight,
+  faLocationDot,
+  faArrowUpRightFromSquare
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faCircleQuestion,
+  faCreditCard,
+} from "@fortawesome/free-regular-svg-icons";
+import {
+    faFacebookF,faYoutube,faInstagram
+  } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+const app = createApp(App).component("font-awesome-icon", FontAwesomeIcon);
+
+library.add(
+  faCircleQuestion,faInstagram,
+  faArrowUpRightFromSquare,faFacebookF,
+  faCreditCard,
+  faLocationDot,
+  faYoutube,
+  faBars,
+  faUser,
+  faMagnifyingGlass,
+  faBasketShopping,
+  faArrowLeft,
+  faXmark,
+  faGlobe,
+  faShieldHalved,
+  faTruck,
+  faRotateRight
+);
+
+app.use(router);
+
+app.mount("#app");
