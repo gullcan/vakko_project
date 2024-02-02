8a8360ac9b963f7a4eee5f03ecf5b792e6614a29
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:53:10 +0300

    Create HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import TheWelcome from '../components/WelcomeItem.vue'
+
+</script>
+
+<template>
+  <main>
+    <a href="" style="--padding-bottom:1rem;--desktop-padding-top:0rem;" class="vn-hero-slider">
+   <div class="clp-cover">
+     <div class="clp-cover-media se-item se-ready" data-se-anim="clpCover" style="position: relative;">
+       
+       <picture>
+         <source media="(max-width: 1024px)" srcset="https://3491f5.cdn.akinoncloud.com/cms/2023/09/26/b00a5dfb-c39a-4a15-9ede-4f390b228f61_size1600x914_cropCenter.jpg 1x, https://3491f5.cdn.akinoncloud.com/cms/2023/09/26/e2b0e8bc-253c-49e4-8422-d8c5b54589b0_size768x1182_cropCenter.jpg 2x">
+         <img class="" src="https://3491f5.cdn.akinoncloud.com/cms/2023/09/26/b00a5dfb-c39a-4a15-9ede-4f390b228f61_size1600x914_cropCenter.jpg" alt="None">
+       </picture>
+       
+     </div>
+      <div style=" color:#ffffff " class="clp-cover-title">
+       
+   </div>
+   
+   </div>
+ </a>
+  </main>
+</template>
