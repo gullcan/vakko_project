2e625e0485c16f8dd068bad3de70d0d57adb8226
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:53:13 +0300

    Create LoginForm.vue
    firebase işlemleri

---
+++ src/views/Login/LoginForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="usr">
+      <div class="usr-body">
+        <PzForm id="LoginForm" class="login-form pz-form" service="User" action="login">
+        </PzForm>
+      </div>
+      <div class="usr-media" style="background-image: url(https://fbfad05d.cdn.akinoncloud.com/static_omnishop/shopi-28/img/main_slide_login_2.jpg);">
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+
+  import PzForm from './PzForm.vue';  // PzForm bileşeni
+
+  export default defineComponent({
+    components: {
+      PzForm
+    },
+
+  });
+  </script>
+  
+  
