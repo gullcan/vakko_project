5301381fb4665dde54404dc735f0a27929d78a5b
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:52:29 +0300

    Create SocialMediaC.vue
    firebase işlemleri

---
+++ src/components/SocialMediaC.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="ftr-social">
+
+
+<a href="https://www.facebook.com/Vakko" target="_blank">
+    <font-awesome-icon class="icon" :icon="['fab', 'facebook-f']" />
+</a>
+
+
+
+<a href="https://www.youtube.com/user/ModaVakkodur" target="_blank">
+    <font-awesome-icon class="icon" :icon="['fab', 'youtube']" />
+</a>
+
+
+
+<a href="https://www.instagram.com/vakko/" target="_blank">
+    <font-awesome-icon class="icon" :icon="['fab', 'instagram']" />
+</a>
+
+
+
+<a style="display: flex;justify-content: center;align-items: center;"
+    href="https://twitter.com/ModaVakkodur" target="_blank">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
+        <g id="surface1">
+            <path style=" stroke:none;fill-rule:nonzero;fill:rgb(0%,0%,0%);fill-opacity:1;"
+                d="M 14.285156 10.171875 L 23.222656 0 L 21.105469 0 L 13.34375 8.832031 L 7.148438 0 L 0 0 L 9.371094 13.355469 L 0 24.019531 L 2.117188 24.019531 L 10.308594 14.691406 L 16.851562 24.019531 L 24 24.019531 M 2.878906 1.5625 L 6.132812 1.5625 L 21.101562 22.535156 L 17.851562 22.535156 ">
+            </path>
+        </g>
+    </svg>
+</a>
+
+
+
+
+</div>
+</template>
