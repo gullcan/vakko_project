93fca5af9761ab856323d89d81ebc538fb5feb88
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:52:24 +0300

    Create SearchBox.vue
    firebase işlemleri

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,36 @@
+<div class="sbx" id="searchbox">
+    <div class="sbx-header">
+      <div class="sbx-form">
+        <button class="sbx-button" type="button" aria-label="Ara">
+          <svg class="icon icon-search">
+            <use xlink:href="#search"></use>
+          </svg>
+        </button>
+        <div class="sbx-input">
+          <input type="text" placeholder="Ürün, kategori, marka...">
+        </div>
+      </div>
+      <button class="sbx-close" type="button" aria-label="Kapat">
+        <svg class="icon icon-close">
+          <use xlink:href="#close"></use>
+        </svg>
+      </button>
+    </div>
+    <div class="sbx-body">
+      <div class="sbx-categories">
+        <ul class="ul-clear">
+          <li><a class="link" href="#">Bermuda &amp; Şort</a></li>
+          <li><a class="link" href="#">Beccar Couture</a></li>
+          <li><a class="link" href="#">Atkı &amp; Bere</a></li>
+        </ul>
+      </div>
+
+      <div id="target-sbx-products" class="sbx-products">
+
+      </div>
+      <div class="sbx-more">
+        <button class="sbx-btn js-route-search" type="button" title="Daha Fazla Göster"><span>Daha Fazla Göster</span>
+        </button>
+      </div>
+    </div>
+  </div>
