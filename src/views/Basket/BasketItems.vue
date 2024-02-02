98d4f6c6f85339b5525cabed7069e652d9547fed
gülcan <130061481+gullcan@users.noreply.github.com>
2024-02-02 12:52:55 +0300

    Create BasketItems.vue
    firebase işlemleri

---
+++ src/views/Basket/BasketItems.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="basket-items js-basket-list">
+      <div class="basket-vakkocard__notify" style="padding-left: 0;">
+        <i class="pz-icon-information"></i>
+        Vakko kart indirim oranlarını görmek için üye girişi yapmalısınız.
+      </div>
+  
+      <div class="cart">
+        <div class="cart-rows js-basket-product-list"></div>
+      </div>
+  
+      <template class="js-basket-product-template" hidden="">
+        <!-- ... (other template content) ... -->
+      </template>
+  
+      <div class="cart-footer">
+        <a class="link-inline" href="/">ALIŞVERİŞE DEVAM ET</a>
+      </div>
+    </section>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    // ... (component logic) ...
+  });
+  </script>
+  
